20ba3a94655b9c1a000b5fa904ea8139743a1678
manjilkoirala <koiralamanjil7@gmail.com>
2025-01-09 10:08:31 +0545

    implement authentication module with signup and login functionality, add Prisma integration, and configure database connection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  login() {
-    return { message: 'I have logged in' };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    //generate the password hash
+    const hash = await argon.hash(dto.password);
+    //save the user to the database
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      //return the user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
-  signup() {
-    return { message: 'I have signed up' };
+
+  async login(dto: AuthDto) {
+    //find the user by email
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //if user does not exist throw exception
+    if (!user) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    //compare password
+    const pwMatch = await argon.verify(user.hash, dto.password);
+
+    //if password does not match throw exception
+    if (!pwMatch) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    //send back the user
+    delete user.hash;
+    return user;
   }
 }
